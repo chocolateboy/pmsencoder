b64488404f463dcaefe7b37840f1591c2150dcb4
chocolateboy <chocolate@cpan.org>
2010-09-07 23:48:34 +0100

    rm TODONE

--- TODO.groovy
+++ TODO.groovy
@@ -2,11 +2,6 @@
 
 /* XPath scraping? */
 
-    scrape '//foo/bar/@baz', 'foo:(?<bar>bar):baz'
-    scrape '//foo/bar/text()', 'foo:(?<bar>bar):baz'
-
-/* Or: */
-
     scrape(
         uri:    uri, // default
         xpath:  '//foo/bar/@baz',
@@ -14,34 +9,4 @@
         format: 'html' // default if xpath is defined
     )
 
-/* Add tests for ytaccept */
-
-/* add the list of matched profiles to the command object so that it can be queried */
-
-    pattern ('Custom Youtube') {
-        match { 'YouTube' in matched }
-        match { [ 'YouTube', 'YouTube HD' ] in matched }
-    }
-
-/*
-    use a subclass of GroovyShell that doesn't auto-import java.net.URI or net.pms.PMS so that they
-    can be used as a stash var. then make all core vars upper case to distinguish them from user vars:
-
-        ARGS
-        EXECUTABLE
-        OUTPUT
-	PMS
-        URI
-
-    Alternatively (and much more lazily) use a dollar prefix (need to check that this works with RegexPlus):
-*/
-
-	match $URI: '(?<$URI>http://www.example.com/foo/bar/1234/)unused.xyz'
-
-	$URI = 'http://...'
-
-/* expose a PMS instance - and use it to set nbcores */
-
-    def nbcores = PMS.getConfiguration().getNumberOfCpuCores()
-
-    DEFAULT_MENCODER_ARGS = [ ..., '...:threads=$nbcores:...', ... ]
+/* add tests for ytaccept */
--- examples/politiek24.groovy
+++ examples/politiek24.groovy
@@ -1,12 +1,12 @@
 config {
     profile ('Politiek 24') {
         pattern {
-            match $URI: '^http://livestreams\\.omroep\\.nl/nos/politiek24-bb$'
+            match { $URI == 'http://livestreams.omroep.nl/nos/politiek24-bb' }
         }
 
         action {
             // grab the .asx file and extract the first stream into $URI
-	    // FIXME - dollar in group name doesn't work with current (2010-09-07) version of RegExPlus
+            // FIXME - dollar in group name doesn't work with current (2010-09-07) version of RegExPlus
             scrape '<Ref\\s+href="(?<$URI>[^"]+)"\\s*/>'
         }
     }
