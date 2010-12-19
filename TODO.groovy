471bcda98293abd428f0f2183e23d84890405f08
chocolateboy <chocolate@cpan.org>
2010-12-19 08:44:14 +0000

    update changelog rm TODONE

--- TODO.groovy
+++ TODO.groovy
@@ -14,7 +14,7 @@
 /* make scripts hot-swappable */
 
 /*
-    wrap profile/config evaluation in try/catch blocks so that errors can be recovered from
+    wrap profile/script evaluation in try/catch blocks so that errors can be recovered from
 */
 
 /*
@@ -48,53 +48,3 @@
 
         private static final def URI = "URI"
 */
-
-// add an option to allow the logfile location to be specified
-
-// pmsencoder_conf=/path/to/pmsencoder.conf
-
-// allow $DOWNLOADER to be a string (i.e. tokenize it if it's not a list)
-
-// single (String) argument domain method - default to matching the URI
-
-    domain 'example.com'
-
-// or just remove the need to supply $URI altogether (what else would it match?)
-
-/*
-
-    wire dependencies lazily so that we don't have to re-order/predeclare profiles
-
-    ditto replacements?
-
-    What if something now declares itself as coming after/before (e.g.) YouTube DL?
-
-    FIXME: need some sort of alias map for replacements:
-
-    if we have TED and YouTube:
-
-    profiles:
-
-        key:     profile
-        TED:     TED
-        YouTube: YouTube
-
-    - and then replace YouTube with YouTube DL:
-
-    profiles:
-
-        key:     profile
-        TED:     TED
-        YouTube: YouTube DL
-
-    aliases:
-
-        profile:    key
-        YouTube DL: YouTube
-
-    XXX We need a topological (rather than lexical) ordering for replacements
-
-    resolve all this in the validate/resolve method (currently Config.verifyDependencies) called from Config.match
-*/
-
-// use maven-external-dependency-plugin to obviate the need to locally install PMS and RegExPlus
