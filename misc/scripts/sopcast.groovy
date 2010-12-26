e8b1860c25e06fd0da097c5353a843dd2038844f
chocolateboy <chocolate@cpan.org>
2010-12-26 09:12:17 +0000

    + credit

--- misc/scripts/sopcast.groovy
+++ misc/scripts/sopcast.groovy
@@ -1,3 +1,7 @@
+// this is a tweaked version of a script by hans_gregor:
+// http://ps3mediaserver.org/forum/viewtopic.php?f=6&t=8776&p=41615#p41612
+// change /path/to/sopcast to the path to the sopcast binary to use it
+
 script {
     def SURI = 'SOPCAST_URI'
     def SLOO = 'SOPCAST_LOOPBACK'
@@ -21,7 +25,7 @@ script {
         }
 
         action {
-            $URI = "http://127.0.0.1:8902/stream"
+            $URI = 'http://127.0.0.1:8902/stream'
         }
     }
 
