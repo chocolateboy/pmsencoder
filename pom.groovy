2d2b1a1b508328e37846e730ac6088503a95a3a1
chocolateboy <chocolate@cpan.org>
2011-01-24 01:09:25 +0000

    update Groovy++ to 0.4.150 rm unused dependency

--- pom.groovy
+++ pom.groovy
@@ -8,7 +8,7 @@ project {
 
     properties {
         'project.build.sourceEncoding' 'UTF-8'
-        'groovypp-version' '0.4.119'
+        'groovypp-version' '0.4.150'
         'groovypp-repo' 'http://groovypp.artifactoryonline.com/groovypp'
         'junit-version' '4.8.2'
     }
@@ -94,12 +94,6 @@ project {
             artifactId 'junit'
             version '${junit-version}'
             scope 'test'
-        }
-
-        dependency {
-            groupId 'excalibur'
-            artifactId 'excalibur-fortress'
-            version '1.0'
         }
 
         dependency {
