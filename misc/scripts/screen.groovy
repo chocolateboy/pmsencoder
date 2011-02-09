c1c054db4614f7ae353e003dd146292f602deec2
chocolateboy <chocolate@cpan.org>
2011-02-09 19:20:22 +0000

    tweak screen script

--- misc/scripts/screen.groovy
+++ misc/scripts/screen.groovy
@@ -7,13 +7,14 @@ script {
         }
 
         action {
+            // $PARAMS.waitbeforestart = 0
             $TRANSCODER = [
                 'cvlc',
                 'screen://',
-                '--screen-fps',     '25', // limited to MPEG-2 framerates
+                '--screen-fps',     '5',
                 '--screen-caching', '100',
                 '--sout',
-                    '#transcode{vcodec=mp2v,vb=512,scale=1,width=1024,height=768,acodec=none}:' +
+                    '#transcode{vcodec=mp2v,fps=25,vb=512,scale=1,width=1024,height=768,acodec=none}:' +
                     "duplicate{dst=std{access=file,mux=ts,dst=\"${$TRANSCODER_OUT}\"}}"
             ]
         }
