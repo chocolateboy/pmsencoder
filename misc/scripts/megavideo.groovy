2391a4153777d685de52970cf6e2acb364395db0
chocolateboy <chocolate@cpan.org>
2011-02-09 20:48:34 +0000

    Megavideo script also requires PMSEncoder >= 1.4.0 (for $FFMPEG)

--- misc/scripts/megavideo.groovy
+++ misc/scripts/megavideo.groovy
@@ -1,3 +1,4 @@
+// XXX this script requires PMSEncoder >= 1.4.0
 // XXX this script needs to be loaded before get_flash_videos.groovy
 // FIXME: for now, the easiest way to ensure that is to save this as INIT.groovy
 
