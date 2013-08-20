89b51a3e98382436a9bc9800e274d8d77faa71dc
chocolateboy <chocolate@cpan.org>
2013-08-20 05:10:15 +0100

    Event: clean up names

--- src/main/groovy/com/chocolatey/pmsencoder/Event.groovy
+++ src/main/groovy/com/chocolatey/pmsencoder/Event.groovy
@@ -2,14 +2,14 @@ package com.chocolatey.pmsencoder
 
 @groovy.transform.CompileStatic
 enum Event {
-    TRANSCODE {
-        public String toString() {
-            "launchTranscode"
-        }
-    },
-    FINALIZE {
-        public String toString() {
-            "finalizeTranscoderArgs"
-        }
+    TRANSCODE("launchTranscode"),
+    FINALIZE("finalizeTranscoderArgs");
+
+    private String name
+
+    private Event(String name) {
+        this.name = name
     }
+
+    public String toString() { name }
 }
