513e181065e72c716be4f9ad146e85df8a925119
chocolateboy <chocolate@cpan.org>
2013-11-22 05:39:29 +0000

    add support for The Daily Show + The Colbert Report

---
+++ src/main/resources/scripts/youtube_dl_concat.groovy
@@ -0,0 +1,31 @@
+// videofeed.Web,Comedy Central=http://extechops.net/full-episode-feeds/daily-show.rss
+// videofeed.Web,Comedy Central=http://extechops.net/full-episode-feeds/colbert-report.rss
+
+script (INIT) {
+    def SEPARATOR = System.getProperty('line.separator')
+    def FILENAME = 'pmsencoder_ffmpeg_concat_%d_%d.txt'
+    def TEMP_FOLDER = pms.getConfiguration().getTempFolder()
+
+    profile ('YouTube-DL Concat') {
+        pattern {
+            match { YOUTUBE_DL_PATH }
+            domains([ 'thedailyshow.com', 'colbertnation.com' ])
+        }
+
+        action {
+            def uris = (YOUTUBE_DL_PATH + [ '-g', uri ]).execute().text.readLines()
+
+            logger.debug("URIs: ${uris.inspect()}")
+
+            def files = uris.collect({ "file '$it'" }).join(SEPARATOR)
+            def filename = String.format(FILENAME, Thread.currentThread().getId(), System.currentTimeMillis())
+            def file = new File(TEMP_FOLDER, filename)
+
+            logger.trace("concat file: ${file.getAbsolutePath()}")
+            file.write(files)
+            file.deleteOnExit()
+            set '-f': 'concat'
+            uri = file.getAbsolutePath()
+        }
+    }
+}
